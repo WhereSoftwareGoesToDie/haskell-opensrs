3faca3b41aaea48fddaf68a932bb70db95e233e5
Geoffrey Roberts <groberts@anchor.net.au>
2014-11-06 16:16:58 +1100

    Added a little test suite so we can be sure that our objects get packed into XML

---
+++ tests/XmlTests.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Blaze.ByteString.Builder
+import qualified Data.ByteString.Lazy.Char8 as BSL8
+import Data.Time
+import Data.Map
+import Data.OpenSRS.Types
+import Data.OpenSRS.ToXML
+import Test.Hspec
+import Test.Hspec.Expectations
+import Text.XmlHtml
+
+testConfig :: SRSConfig
+testConfig = SRSConfig "http://localhost:55443" "janedoe" "0123456789abcdef" "127.0.0.1"
+
+testDomain1 :: IO Domain
+testDomain1 = do
+    t <- getCurrentTime
+    let t' = addUTCTime ((86400 * 365) :: NominalDiffTime) t
+    return $ Domain "foo.com" True contacts (Just t) Nothing (Just t) (Just "5534") (Just t') True (Just t') nameservers
+  where
+    contacts = fromList [
+        ("owner", testc),
+        ("admin", testc),
+        ("billing", testc),
+        ("tech", testc)
+        ]
+    testc = Contact (Just "Jane")
+                    (Just "Doe")
+                    (Just "Frobozz Pty Ltd")
+                    (Just "jane.doe@frobozz.com.au")
+                    (Just "+61.299999999")
+                    Nothing
+                    (Just "Frobozz Pty Ltd")
+                    (Just "Level 50")
+                    (Just "1 George Street")
+                    (Just "Sydney")
+                    (Just "NSW")
+                    (Just "2000")
+                    (Just "AU")
+    nameservers = [
+        Nameserver (Just "ns1.anchor.net.au") (Just "0") (Just "127.0.0.1"),
+        Nameserver (Just "ns2.anchor.net.au") (Just "0") (Just "127.0.0.2") ]
+
+suite :: Spec
+suite = do
+    describe "Domains" $ do
+        it "Can be marshalled into a registration request" $ do
+            d <- testDomain1
+            let req = RegisterDomain testConfig d False Nothing Nothing
+                                     False False True True 1
+                                     "janedoe" "imasecret" "new" Nothing
+            let rxml = BSL8.unpack $ toLazyByteString $ render $ requestXML req
+            rxml `shouldContain` "<OPS_envelope>"
+
+-- | Explicitly pass a test.
+pass :: Expectation
+pass = return ()
+
+main :: IO ()
+main = do
+    res <- hspec suite
+    return ()
+
+
