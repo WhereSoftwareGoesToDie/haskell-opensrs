278833baf4159d757baf2fb709123b67d13e44e0
Geoffrey Roberts <groberts@anchor.net.au>
2014-11-06 18:32:57 +1100

    lookup and get tests should now work as expected

--- tests/RequestTests.hs
+++ tests/RequestTests.hs
@@ -4,6 +4,7 @@ module Main where
 
 import Blaze.ByteString.Builder
 import qualified Data.ByteString.Lazy.Char8 as BSL8
+import Data.Either.Utils
 import Data.Map
 import Data.OpenSRS
 import Data.OpenSRS.Types
@@ -18,16 +19,28 @@ suite :: Spec
 suite = do
     describe "Domains" $ do
         it "looks up a valid free domain" $ do
-            let req = LookupDomain testConfig lookupDomain
+            let req = LookupDomain testConfig lookupDomainAvailable
             res <- doRequest req
-            putStrLn $ show res
-            pass
+            res `shouldBe` (Right $ DomainAvailabilityResult $ Available lookupDomainAvailable)
+
+        it "looks up a valid taken domain" $ do
+            let req = LookupDomain testConfig lookupDomainUnavailable
+            res <- doRequest req
+            res `shouldBe` (Right $ DomainAvailabilityResult $ Unavailable lookupDomainUnavailable)
 
         it "gets a valid existing domain" $ do
-            let req = GetDomain testConfig getDomain
+            let req = GetDomain testConfig getDomainOwned
             res <- doRequest req
-            putStrLn $ show res
-            pass
+            case res of
+                Right (DomainResult d) -> do
+                    (domainName d) `shouldBe` getDomainOwned
+                Left e                      -> error $ e
+                _                           -> error "This should never happen."
+
+        it "gets a valid existing domain that we don't own" $ do
+            let req = GetDomain testConfig getDomainNotOurs
+            res <- doRequest req
+            (fromLeft res) `shouldContain` "415: Authentication Error."
 
 -- | Explicitly pass a test.
 pass :: Expectation
